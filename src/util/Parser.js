6fa76a346b639022428dbe90a5e34e2a3f011c94
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-27 21:02:50 +0900

    feat: toInteger 함수 구현

--- src/util/Parser.js
+++ src/util/Parser.js
@@ -5,4 +5,29 @@ export default class Parser {
         }
         return raw.split(',').map(name => name.trim());
     }
+
+    static _parseNumberInput(num) {
+        if (Number.isInteger(num)) {
+            return num;
+        }
+        return NaN;
+    }
+
+    static _parseStringInput(str) {
+        const n = Number.parseInt(str, 10);
+        if (Number.isNaN(n)) {
+            return NaN;
+        }
+        return n;
+    }
+
+    static toInteger(raw) {
+        if (typeof raw === 'number') {
+            return this._parseNumberInput(raw);
+        }   
+        if (typeof raw !== 'string') {
+            return this._parseStringInput(raw);
+        }
+        return NaN;
+    }
 }
