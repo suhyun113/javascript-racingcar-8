3ee8e6a3a7542e8b3b4b00e14e67ad24bff54424
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-27 22:31:22 +0900

    feat: 모듈 연동 및 실행 로직 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import InputView from './io/InputView.js';
+import Parser from './util/Parser';
+import InputValidator from './util/InputValidator';
+import GameService from './service/GameService.js';
+
 class App {
-  async run() {}
+  async run() {
+    const rawNames = await InputView.readCarNames();
+    const rawTryCount = await InputView.readTryCount();
+
+    const names = Parser.splitNames(rawNames);
+    const tryCount = Parser.toInteger(rawTryCount);
+
+    InputValidator.validateNames(names);
+    InputValidator.validateTryCount(tryCount);
+
+    await GameService.play({ names, tryCount });
+  }
 }
 
 export default App;
